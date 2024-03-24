409c330ab5952516532900ee039db76ed56e903d
Aditya Gupta <72199742+Adityagupta1625@users.noreply.github.com>
2024-03-24 10:31:07 +0530

    feat: changed validators from class-validators to ajv

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
 
-
 process.on('uncaughtException', (err: any) => {
   console.error(err)
   process.exit(1)
--- src/utils/baseValidators.ts
+++ src/utils/baseValidators.ts
@@ -1,28 +1,29 @@
-import { plainToInstance } from 'class-transformer'
-import { validateOrReject } from 'class-validator'
+import Ajv, { type Schema } from 'ajv'
 import { type NextFunction, type Request, type Response } from 'express'
+import HttpException from './HttpException'
 
-export abstract class BaseValidator {
-  private readonly inputObject: any;
+export class BaseValidator {
+  private readonly schemaObj: Schema
 
-  constructor(inputObject: any) {
-    this.inputObject=inputObject
+  constructor (schemaObj: Schema) {
+    this.schemaObj = schemaObj
   }
 
-  
-  public async validateInput(
+  public async validateInput (
     req: Request,
     res: Response,
     next: NextFunction
   ): Promise<Response | any> {
     try {
-      const dtoInstance = plainToInstance(this.inputObject, req.body)
+      const ajv = new Ajv()
 
-      await validateOrReject(dtoInstance)
-
-      next()
+      const validate = ajv.compile(this.schemaObj)
+      const valid = validate(req.body.data)
+      if (valid) next()
+      else throw new HttpException(400, validate.errors)
     } catch (e: any) {
-      return res.status(400).json({ message: e })
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      return res.status(e?.errorCode ?? 500).json({ message: e })
     }
   }
 }
